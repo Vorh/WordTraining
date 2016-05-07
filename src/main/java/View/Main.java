3f22f04a88fae782855249f341dd1cc6135db66b
Vorh <sorcerer12@list.ru>
2016-05-07 13:48:01 +0500

    add TabPane and three Main Tab

--- src/main/java/View/Main.java
+++ src/main/java/View/Main.java
@@ -1,7 +1,47 @@
 package View;
 
-public class Main {
-    public static void main(String[] args) {
-        
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setWidth(400);
+        primaryStage.setHeight(500);
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("WordTraining");
+
+        TabPane tabPane = new TabPane();
+        tabPane.setTranslateY(0);
+
+        Tab task = new Tab("Задача");
+        task.setClosable(false);
+        Pane paneTask = new Pane();
+        paneTask.setPrefSize(400, 500);
+        task.setContent(paneTask);
+
+        Tab statistics = new Tab("Статистика");
+        statistics.setClosable(false);
+        Pane paneSt = new Pane();
+        paneSt.setPrefSize(400,500);
+        statistics.setContent(paneSt);
+
+        Tab addWord = new Tab("Добавление слова");
+        addWord.setClosable(false);
+        Pane paneWord = new Pane();
+        paneWord.setPrefSize(400,500);
+        addWord.setContent(paneWord);
+
+        tabPane.getTabs().addAll(task,statistics,addWord);
+
+        Scene scene = new Scene(tabPane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
     }
 }
