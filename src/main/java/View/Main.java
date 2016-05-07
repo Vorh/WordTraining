468d8c49ccf6c753d0ddbae8df27e1985acc3c12
Vorh <sorcerer12@list.ru>
2016-05-07 14:35:14 +0500

    add fourth Tab and components pane addWord

--- src/main/java/View/Main.java
+++ src/main/java/View/Main.java
@@ -2,10 +2,11 @@ package View;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
 
 public class Main extends Application{
 
@@ -16,6 +17,15 @@ public class Main extends Application{
         primaryStage.setResizable(false);
         primaryStage.setTitle("WordTraining");
 
+        TabPane tabPane = getTabPane();
+
+        Scene scene = new Scene(tabPane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+    private TabPane getTabPane() {
         TabPane tabPane = new TabPane();
         tabPane.setTranslateY(0);
 
@@ -33,15 +43,54 @@ public class Main extends Application{
 
         Tab addWord = new Tab("Добавление слова");
         addWord.setClosable(false);
-        Pane paneWord = new Pane();
-        paneWord.setPrefSize(400,500);
+        Pane paneWord = getPaneAddWord();
         addWord.setContent(paneWord);
 
-        tabPane.getTabs().addAll(task,statistics,addWord);
+        Tab setting = new Tab("Настройка");
+        setting.setClosable(false);
+        Pane paneSetting = new Pane();
+        paneSetting.setPrefSize(400,500);
+        setting.setContent(paneSetting);
 
-        Scene scene = new Scene(tabPane);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        tabPane.getTabs().addAll(task,statistics,addWord,setting);
+        return tabPane;
+    }
 
+    private Pane getPaneAddWord() {
+        Pane paneWord = new Pane();
+        paneWord.setPrefSize(400,500);
+
+        Text text1 = new Text("Введите слово");
+        text1.setTranslateX(15);
+        text1.setTranslateY(20);
+
+        TextField wordInput = new TextField();
+        wordInput.setTranslateX(15);
+        wordInput.setTranslateY(40);
+
+        Text text2 = new Text("Выберите категорию");
+        text2.setTranslateX(15);
+        text2.setTranslateY(80);
+
+        ComboBox boxCategory = new ComboBox();
+        boxCategory.setTranslateX(15);
+        boxCategory.setTranslateY(100);
+
+        Text text3 = new Text("Установить приоритет");
+        text3.setTranslateX(15);
+        text3.setTranslateY(140);
+
+        TextField priority = new TextField();
+        priority.setTranslateX(15);
+        priority.setTranslateY(160);
+
+        Button but = new Button("Добавить");
+        but.setTranslateX(150);
+        but.setTranslateY(350);
+
+
+        paneWord.getChildren().addAll(text1,text2,text3,
+                                   but,wordInput,priority,boxCategory);
+        return paneWord;
     }
 }
