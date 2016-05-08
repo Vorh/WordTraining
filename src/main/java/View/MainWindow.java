17c5e3edcedf9dca1a89f2c850fa3c33c7d26975
Vorh <sorcerer12@list.ru>
2016-05-08 20:16:30 +0500

    added class Statistics Table

--- src/main/java/View/MainWindow.java
+++ src/main/java/View/MainWindow.java
@@ -2,18 +2,15 @@ package View;
 
 import Model.ManagerXML;
 import Model.Word;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
-import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
-
-import java.util.List;
 
 public class MainWindow {
 
@@ -22,14 +19,8 @@ public class MainWindow {
     private TextField transcription;
     private TextField category;
     private TextField priority;
-    private ObservableList records;
-    private TableColumn name;
-    private TableColumn transferColumn;
-    private TableColumn priorityColumn;
-    private TableColumn categoryColumn;
-    private TableColumn transcriptionColumn;
-    private TableView tableStatistics;
 
+    private StatisticsTable statisticsTable;
 
     public TabPane getTabPane() {
 
@@ -45,8 +36,8 @@ public class MainWindow {
 
         Tab statistics = new Tab("Статистика");
         statistics.setClosable(false);
-        Pane paneSt = getPaneStatistics();
-        statistics.setContent(paneSt);
+        statisticsTable = new StatisticsTable();
+        statistics.setContent(statisticsTable);
 
         Tab addWord = new Tab("Добавление слова");
         addWord.setClosable(false);
@@ -63,96 +54,6 @@ public class MainWindow {
         return tabPane;
     }
 
-    private Pane getPaneStatistics() {
-        Pane paneSt = new Pane();
-        paneSt.setPrefSize(400,450);
-
-        tableStatistics = new TableView();
-        tableStatistics.setEditable(true);
-        tableStatistics.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-        tableStatistics.setPrefSize(395, 400);
-        tableStatistics.setTableMenuButtonVisible(true);
-        name = new TableColumn("Название");
-        transferColumn = new TableColumn("Перевод");
-        transcriptionColumn = new TableColumn("Транскрипция");
-        priorityColumn = new TableColumn("Приоритет");
-        categoryColumn = new TableColumn("Категория");
-
-        records = FXCollections.observableArrayList();
-        List<Word> list = ManagerXML.unmarshaller().getList();
-
-        addWordInTable(list);
-
-
-        name.setCellFactory(TextFieldTableCell.<Word>forTableColumn());
-        name.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Word, String>>() {
-            public void handle(TableColumn.CellEditEvent<Word, String> event) {
-                event.getTableView().getItems().get(event.getTablePosition().getRow()).setName(event.getNewValue());
-                ManagerXML.marshalSave(records);
-            }
-        });
-
-        transferColumn.setCellFactory(TextFieldTableCell.<Word>forTableColumn());
-        transferColumn.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Word, String>>() {
-            public void handle(TableColumn.CellEditEvent<Word, String> event) {
-                event.getTableView().getItems().get(event.getTablePosition().getRow()).setTransfer(event.getNewValue());
-                ManagerXML.marshalSave(records);
-            }
-        });
-
-        priorityColumn.setCellFactory(TextFieldTableCell.<Word>forTableColumn());
-        priorityColumn.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Word, String>>() {
-            public void handle(TableColumn.CellEditEvent<Word, String> event) {
-                event.getTableView().getItems().get(event.getTablePosition().getRow()).setPriority(event.getNewValue());
-                ManagerXML.marshalSave(records);
-            }
-        });
-
-        categoryColumn.setCellFactory(TextFieldTableCell.<Word>forTableColumn());
-        categoryColumn.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Word, String>>() {
-            public void handle(TableColumn.CellEditEvent<Word, String> event) {
-                event.getTableView().getItems().get(event.getTablePosition().getRow()).setCategory(event.getNewValue());
-                ManagerXML.marshalSave(records);
-            }
-        });
-
-        transcriptionColumn.setCellFactory(TextFieldTableCell.<Word>forTableColumn());
-        transcriptionColumn.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Word, String>>() {
-            public void handle(TableColumn.CellEditEvent<Word, String> event) {
-                event.getTableView().getItems().get(event.getTablePosition().getRow()).setTranscription(event.getNewValue());
-                ManagerXML.marshalSave(records);
-            }
-        });
-
-        Button deleteWord = new Button("Удалить слово");
-        deleteWord.setTranslateX(10);
-        deleteWord.setTranslateY(410);
-
-        deleteWord.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            public void handle(MouseEvent event) {
-                int s =  tableStatistics.getSelectionModel().getSelectedIndex();
-                tableStatistics.getItems().remove(s);
-                ManagerXML.marshalSave(records);
-            }
-        });
-
-        tableStatistics.getColumns().addAll(name, transferColumn, priorityColumn, categoryColumn, transcriptionColumn);
-        paneSt.getChildren().addAll(tableStatistics,deleteWord);
-        return paneSt;
-    }
-
-    private void addWordInTable(List<Word> list) {
-        records.clear();
-        for(Word record : list){
-            records.addAll(record);
-            name.setCellValueFactory(new PropertyValueFactory<Word, String>("name"));
-            transferColumn.setCellValueFactory(new PropertyValueFactory<Word, String>("transfer"));
-            priorityColumn.setCellValueFactory(new PropertyValueFactory<Word, String>("priority"));
-            categoryColumn.setCellValueFactory(new PropertyValueFactory<Word, String>("category"));
-            transcriptionColumn.setCellValueFactory(new PropertyValueFactory<Word, String>("transcription"));
-            tableStatistics.setItems(records);
-        }
-    }
 
     private Pane getPaneAddWord() {
         Pane paneWord = new Pane();
@@ -206,7 +107,7 @@ public class MainWindow {
                 transcription.setText("");
                 category.setText("");
 
-                addWordInTable(ManagerXML.unmarshaller().getList());
+                statisticsTable.addWordInTable(ManagerXML.unmarshaller().getList());
             }
         });
 
---
+++ src/main/java/View/StatisticsTable.java
@@ -0,0 +1,119 @@
+package View;
+
+import Model.ManagerXML;
+import Model.Word;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+
+import java.util.List;
+
+public class StatisticsTable extends Pane {
+
+    private ObservableList records;
+    private TableColumn name;
+    private TableColumn transferColumn;
+    private TableColumn priorityColumn;
+    private TableColumn categoryColumn;
+    private TableColumn transcriptionColumn;
+    private TableView tableStatistics;
+
+
+    StatisticsTable(){
+        this.setPrefSize(400, 450);
+
+        tableStatistics = new TableView();
+        tableStatistics.setEditable(true);
+        tableStatistics.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        tableStatistics.setPrefSize(395, 400);
+        tableStatistics.setTableMenuButtonVisible(true);
+        name = new TableColumn("Название");
+        transferColumn = new TableColumn("Перевод");
+        transcriptionColumn = new TableColumn("Транскрипция");
+        priorityColumn = new TableColumn("Приоритет");
+        categoryColumn = new TableColumn("Категория");
+
+        records = FXCollections.observableArrayList();
+        List<Word> list = ManagerXML.unmarshaller().getList();
+
+        addWordInTable(list);
+
+
+        name.setCellFactory(TextFieldTableCell.<Word>forTableColumn());
+        name.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Word, String>>() {
+            public void handle(TableColumn.CellEditEvent<Word, String> event) {
+                event.getTableView().getItems().get(event.getTablePosition().getRow()).setName(event.getNewValue());
+                ManagerXML.marshalSave(records);
+            }
+        });
+
+        transferColumn.setCellFactory(TextFieldTableCell.<Word>forTableColumn());
+        transferColumn.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Word, String>>() {
+            public void handle(TableColumn.CellEditEvent<Word, String> event) {
+                event.getTableView().getItems().get(event.getTablePosition().getRow()).setTransfer(event.getNewValue());
+                ManagerXML.marshalSave(records);
+            }
+        });
+
+        priorityColumn.setCellFactory(TextFieldTableCell.<Word>forTableColumn());
+        priorityColumn.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Word, String>>() {
+            public void handle(TableColumn.CellEditEvent<Word, String> event) {
+                event.getTableView().getItems().get(event.getTablePosition().getRow()).setPriority(event.getNewValue());
+                ManagerXML.marshalSave(records);
+            }
+        });
+
+        categoryColumn.setCellFactory(TextFieldTableCell.<Word>forTableColumn());
+        categoryColumn.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Word, String>>() {
+            public void handle(TableColumn.CellEditEvent<Word, String> event) {
+                event.getTableView().getItems().get(event.getTablePosition().getRow()).setCategory(event.getNewValue());
+                ManagerXML.marshalSave(records);
+            }
+        });
+
+        transcriptionColumn.setCellFactory(TextFieldTableCell.<Word>forTableColumn());
+        transcriptionColumn.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Word, String>>() {
+            public void handle(TableColumn.CellEditEvent<Word, String> event) {
+                event.getTableView().getItems().get(event.getTablePosition().getRow()).setTranscription(event.getNewValue());
+                ManagerXML.marshalSave(records);
+            }
+        });
+
+        Button deleteWord = new Button("Удалить слово");
+        deleteWord.setTranslateX(10);
+        deleteWord.setTranslateY(410);
+
+        deleteWord.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent event) {
+                int s =  tableStatistics.getSelectionModel().getSelectedIndex();
+                tableStatistics.getItems().remove(s);
+                ManagerXML.marshalSave(records);
+            }
+        });
+
+        tableStatistics.getColumns().addAll(name, transferColumn, priorityColumn, categoryColumn, transcriptionColumn);
+        this.getChildren().addAll(tableStatistics,deleteWord);
+    }
+
+    public void addWordInTable(List<Word> list) {
+        records.clear();
+        for(Word record : list){
+            records.addAll(record);
+            name.setCellValueFactory(new PropertyValueFactory<Word, String>("name"));
+            transferColumn.setCellValueFactory(new PropertyValueFactory<Word, String>("transfer"));
+            priorityColumn.setCellValueFactory(new PropertyValueFactory<Word, String>("priority"));
+            categoryColumn.setCellValueFactory(new PropertyValueFactory<Word, String>("category"));
+            transcriptionColumn.setCellValueFactory(new PropertyValueFactory<Word, String>("transcription"));
+            tableStatistics.setItems(records);
+        }
+    }
+
+}
+
